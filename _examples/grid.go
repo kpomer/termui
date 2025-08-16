017b1814c6461b5f0822a840fb691079926cf19b
kpomer <pomerkevin@gmail.com>
2025-08-16 11:19:58 +0300

    added key inputs to grid.go

--- _examples/grid.go
+++ _examples/grid.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT license that can
 // be found in the LICENSE file.
 
+//go:build ignore
 // +build ignore
 
 package main
@@ -66,7 +67,7 @@ func main() {
 	ls.Border = false
 
 	p := widgets.NewParagraph()
-	p.Text = "<> This row has 3 columns\n<- Widgets can be stacked up like left side\n<- Stacked widgets are treated as a single widget"
+	p.Text = "<> This row has 3 columns\n<- Widgets can be stacked up like left side\n<- Stacked widgets are treated as a single widget\n\n\nPress [q](fg:red) to QUIT THE DEMO"
 	p.Title = "Demonstration"
 
 	grid := ui.NewGrid()
